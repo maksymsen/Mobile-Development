7e946a2295414f03478782d502ca566ebcef29dc
Maksym Sen <Maksymsen@hotmail.com>
2021-09-15 15:58:04 -0400

    Added an onClick listener to take the user to the GBC site when they click the imageButton in the activity_main. An intent is created and started.

--- Lab_2/app/src/main/java/com/example/lab_2/MainActivity.java
+++ Lab_2/app/src/main/java/com/example/lab_2/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.lab_2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton imageButton = findViewById(R.id.imageButton);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("https://georgebrown.ca");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
     }
 }
