3856e19e7de4dbcc9c64d46ad19fce39f3584981
Maksym Sen <Maksymsen@hotmail.com>
2021-09-22 16:34:42 -0400

    Fixed MainActivity

--- Lab_3/app/src/main/java/ca/gbc/comp3074/lab_3/MainActivity.java
+++ Lab_3/app/src/main/java/ca/gbc/comp3074/lab_3/MainActivity.java
@@ -3,9 +3,10 @@ package ca.gbc.comp3074.lab_3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
