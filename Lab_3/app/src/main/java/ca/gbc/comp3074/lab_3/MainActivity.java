1c30ab4449b6f6852a8f5900031cf1f05ebcc62d
Maksym Sen <Maksymsen@hotmail.com>
2021-09-22 17:04:20 -0400

    Creating onAttachFragment to create a new bundle with a new key "message" with the string "first load"

--- Lab_3/app/src/main/java/ca/gbc/comp3074/lab_3/MainActivity.java
+++ Lab_3/app/src/main/java/ca/gbc/comp3074/lab_3/MainActivity.java
@@ -1,15 +1,25 @@
 package ca.gbc.comp3074.lab_3;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onAttachFragment(@NonNull Fragment fragment) {
+        super.onAttachFragment(fragment);
+        if (fragment.getId() == R.id.fragContView2) {
+            Bundle bundle = new Bundle();
+            bundle.putString("message", "first load");
+            fragment.setArguments(bundle);
+        }
     }
 }
