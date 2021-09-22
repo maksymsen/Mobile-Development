827745fce56acdf4772521e18cd1fd74fed61125
Maksym Sen <Maksymsen@hotmail.com>
2021-09-22 13:21:51 -0400

    onCreateOptionsMenu and onOptionsItemSelected created for navigating & initialization of the menu.

--- 101010466Assignment1/app/src/main/java/ca/gbc/comp3074/a101010466_assignment_1/MainActivity.java
+++ 101010466Assignment1/app/src/main/java/ca/gbc/comp3074/a101010466_assignment_1/MainActivity.java
@@ -1,14 +1,40 @@
 package ca.gbc.comp3074.a101010466_assignment_1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public EditText numHoursWorked;
+    public EditText hourlyRate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_about:
+                Intent intent = new Intent(this, AboutActivity.class);
+                startActivity(intent);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
