335a6c8b18d9897f2c7f3c744faff9ca828da246
Maksym Sen <Maksymsen@hotmail.com>
2021-09-26 19:16:48 -0400

    Added a labelOutput and resized it in activity_main.xml, created and instantiated output variable in MainActivity

--- Lab2/app/src/main/java/ca/gbc/comp3074/lab2/MainActivity.java
+++ Lab2/app/src/main/java/ca/gbc/comp3074/lab2/MainActivity.java
@@ -3,12 +3,17 @@ package ca.gbc.comp3074.lab2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView output;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        output = findViewById(R.id.labelOutput);
     }
 }
