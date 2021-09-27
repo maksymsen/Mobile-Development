3185bb781b9bafa5a8392dabb4aa8f3295997573
Maksym Sen <Maksymsen@hotmail.com>
2021-09-26 22:26:14 -0400

    Changed folder name for assignment 1

---
+++ Assignment1/app/src/main/java/ca/gbc/comp3074/a101010466_assignment_1/AboutActivity.java
@@ -0,0 +1,14 @@
+package ca.gbc.comp3074.a101010466_assignment_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
