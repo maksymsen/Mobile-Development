268239b3ff322890748cd9c48e232c52b5bcdc35
Maksym Sen <Maksymsen@hotmail.com>
2021-09-26 20:09:04 -0400

    Created variables for the 4 buttons and initialized them.

--- Lab2/app/src/main/java/ca/gbc/comp3074/lab2/MainActivity.java
+++ Lab2/app/src/main/java/ca/gbc/comp3074/lab2/MainActivity.java
@@ -3,11 +3,16 @@ package ca.gbc.comp3074.lab2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView output;
+    Button add;
+    Button minus;
+    Button step;
+    Button reset;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +20,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         output = findViewById(R.id.labelOutput);
+
+        // This may be redundant as the default value for this label should be "0", but just in case, we set it to "0" onCreate anyway.
+        output.setText("0");
+
+        add = findViewById(R.id.btnAdd);
+        minus = findViewById(R.id.btnMinus);
+        step = findViewById(R.id.btnStep);
+        reset = findViewById(R.id.btnReset);
     }
 }
