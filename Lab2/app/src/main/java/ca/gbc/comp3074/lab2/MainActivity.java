f8bf119540a2bed69b73e3c65c59191f2fd55aad
Maksym Sen <Maksymsen@hotmail.com>
2021-09-26 20:47:09 -0400

    Added output and button variables, and initialized them.

--- Lab2/app/src/main/java/ca/gbc/comp3074/lab2/MainActivity.java
+++ Lab2/app/src/main/java/ca/gbc/comp3074/lab2/MainActivity.java
@@ -3,12 +3,43 @@ package ca.gbc.comp3074.lab2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView output;
+    Button add;
+    Button minus;
+    Button step;
+    Button reset;
+    int stepValue = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        output = findViewById(R.id.labelOutput);
+
+        // This may be redundant as the default value for this label should be "0", but just in case, we set it to "0" onCreate anyway.
+        output.setText("0");
+
+        add = findViewById(R.id.btnMinus);
+        minus = findViewById(R.id.btnMinus);
+        step = findViewById(R.id.btnStep);
+        reset = findViewById(R.id.btnReset);
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                output.setText(addToOutput(Integer.parseInt(output.getText().toString()), stepValue));
+            }
+        });
+    }
+
+    public int addToOutput(int value, int step) {
+        return value + step;
     }
 }
