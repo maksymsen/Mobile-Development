ea10f00e0aae0275199c42e6cb56b60cb7cf9ad1
Maksym Sen <Maksymsen@hotmail.com>
2021-10-10 20:52:37 -0400

    Created and initalized variables for the greetings label, the input edit text, the save button. Added an onClick listener for the save button that saves the string value of the EditText and stores it in sharedPrefs.
    loadData() sets the key-value string of the "User" key to the value of the Greetings label, which inherited its value from the EditText. onLoad() simply changes the value of the greeting TextView from "Default Value" to "Greetings, User".

--- Lab4/app/src/main/java/ca/gbc/comp3074/lab4/MainActivity.java
+++ Lab4/app/src/main/java/ca/gbc/comp3074/lab4/MainActivity.java
@@ -2,13 +2,63 @@ package ca.gbc.comp3074.lab4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView userGreeting;
+    Button saveButton;
+    EditText editUserName;
+
+    public static final String SHARED_PREFS = "sharedPrefs";
+    public static final String userValue = "User";
+    public final String greeting = "Greetings, ";
+
+    private String userLoadedUser = "Greetings, User";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editUserName = (EditText) findViewById(R.id.editUserName);
+        saveButton = (Button) findViewById(R.id.saveBtn);
+        userGreeting = (TextView) findViewById(R.id.userGreeting);
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String temp = greeting + editUserName.getText().toString();
+                userGreeting.setText(temp);
+
+                SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+                @SuppressLint("CommitPrefEdits") SharedPreferences.Editor editor = sharedPreferences.edit();
+
+                editor.putString(userValue, userGreeting.getText().toString());
+                editor.apply();
+
+                Toast.makeText(MainActivity.this, "User saved", Toast.LENGTH_SHORT).show();
+            }
+        });
+        loadData();
+        onLoad();
+    }
+
+    public void loadData() {
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        userLoadedUser = sharedPreferences.getString(userValue, "");
+    }
+
+    public void onLoad() {
+        userGreeting.setText(userLoadedUser);
     }
 }
